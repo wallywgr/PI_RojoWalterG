d9b2b47719b7db509fa5d3b2bd52b74da90b985f
Walter Gustavo Rojo <wallywgr@gmail.com>
2023-01-18 11:25:26 -0300

    Modificaciones en el código

--- BackEnd/src/main/java/com/portfolio/wgr/Entity/Persona.java
+++ BackEnd/src/main/java/com/portfolio/wgr/Entity/Persona.java
@@ -17,13 +17,13 @@ public class Persona {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    @Size(min = 1, max = 50, message = "No cumnple con la longitud")
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
     private String nombre;
     @NotNull
-    @Size(min = 1, max = 50, message = "No cumnple con la longitud")
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
     private String apellido;
     
-    @Size(min = 1, max = 50, message = "No cumnple con la longitud")
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
     private String img;  
     
 }
--- BackEnd/src/main/java/com/portfolio/wgr/Security/Enums/RolNombre.java
+++
@@ -1,6 +0,0 @@
-
-package com.portfolio.wgr.Security.Enums;
-
-public enum RolNombre {
-    ROLE_ADMIN,ROLE_USER;
-}
